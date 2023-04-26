f658fa725e6a35f9913020fc8ef8aeeeca89140f
log_ming <736704198@qq.com>
2023-04-27 02:24:56 +0800

    123

--- 7_JS逆向1/02_网络爬虫逆向（有道翻译）.py
+++ 7_JS逆向1/02_网络爬虫逆向（有道翻译）.py
@@ -1,6 +1,10 @@
 import execjs
 import requests
 import time
+from Crypto.Cipher import AES
+from Crypto.Util import Padding
+from hashlib import md5
+import base64
 
 # 利用JS获取sign值
 with open('02_网络爬虫逆向（有道翻译）.js', 'r', encoding='UTF-8') as f:
@@ -18,8 +22,10 @@ headers = {
     'Referer': 'https://fanyi.youdao.com/'
 }
 
+word = '今天星期几？'
+
 data = {
-    'i': '今天天气真好',
+    'i': word,
     'from': 'auto',
     'to':'auto' ,
     'domain': '0',
@@ -34,10 +40,42 @@ data = {
     'mysticTime': timestamp,
     'keyfrom': 'fanyi.web'
 }
-print(data)
+
 response = requests.post(trans_api, headers=headers, data=data)
 print(response.text)
+result = response.content
+
+key_obj = md5()
+key = 'ydsecret://query/key/B*RGygVywfNBwpmBaZg*WT7SIOUP2T0C9WHMZN39j^DAdaZhAnxvGcCY6VYFwnHl'
+key_obj.update(key.encode('utf-8'))
+md5_key = key_obj.digest()
+
+iv_obj = md5()
+iv = 'ydsecret://query/iv/C@lZe2YzHtZ2CYgaXKSVfsb7Y4QWHjITPPZ0nQp87fBeJ!Iv6v^6fvi2WN@bYpJ4'
+iv_obj.update(iv.encode('utf-8'))
+iv_key = iv_obj.digest()
+
+aes = AES.new(key=md5_key, mode=AES.MODE_CBC, IV=iv_key)
+
+base64_result = base64.b64decode(result)
+i = 1
+print(aes.decrypt(base64_result[16 * 0:16 * 1])
+      + aes.decrypt(base64_result[16 * 1:16 * 2])
+      + aes.decrypt(base64_result[16 * 2:16 * 3])
+      + aes.decrypt(base64_result[16 * 3:16 * 4])
+      + aes.decrypt(base64_result[16 * 4:16 * 5])
+      + aes.decrypt(base64_result[16 * 5:16 * 6])
+      + aes.decrypt(base64_result[16 * 6:16 * 7])
+      + aes.decrypt(base64_result[16 * 7:16 * 8])
+      )
+
+#
+# max_num = len(base64_result) // 16
+# all_res = b''
+# for i in range(max_num):
+#     all_res += aes.decrypt(base64_result[16*i:16*(i+1)])
+# all_res += aes.decrypt(Padding.pad(base64_result[16*(i+1):], 16))
+# print(all_res)
 
 
-
-
+print(aes.decrypt(Padding.pad(base64_result, 16)))
--- 7_JS逆向1/02_网络爬虫逆向（有道词典2）.js
+++ 7_JS逆向1/02_网络爬虫逆向（有道词典2）.js
@@ -1,15 +1,27 @@
-function fn(t,o,n){
-    if (!t)
-        return null;
-    const a = e.alloc(16, g(o))
-      , c = e.alloc(16, g(n))
-      , i = r.a.createDecipheriv("aes-128-cbc", a, c);
-    let s = i.update(t, "base64", "utf-8");
-    return s += i.final("utf-8"),
-    s
+const md5 = require('md5-node')
+const CryptoJS = require('crypto-js')
+
+function fn(e){
+    const hash = md5(e);
+    return Buffer.from(hash, 'hex');
 }
+
+function c(t, e, n) {
+    if (t = t.toLowerCase(),
+    o[t])
+        return i.createDecipheriv(t, e, n);
+    if (s[t])
+        return new r({
+            key: e,
+            iv: n,
+            mode: t,
+            decrypt: !0
+        });
+    throw new TypeError("invalid suite type")
+}
+
 t1 = '_jsUyA02rwkOJ4enKX7c4dhd7CjvGkcKfbRx0BjNGW8OgIRH1rgaj5tPhWMKWNW-K3LvAiHRuWyRQBuU0gsuSO1UMxOFFDAcuWENI9r5kAC_7uF2IYJB6c0tWv4hIs7AuFlzgAB95o1_JgWV7d1N9DJdD-laTCZ5Hl57jDPB_l4='
 o1 = "ydsecret://query/key/B*RGygVywfNBwpmBaZg*WT7SIOUP2T0C9WHMZN39j^DAdaZhAnxvGcCY6VYFwnHl"
 n1 = "ydsecret://query/iv/C@lZe2YzHtZ2CYgaXKSVfsb7Y4QWHjITPPZ0nQp87fBeJ!Iv6v^6fvi2WN@bYpJ4"
 
-console.log(fn(t1,o1,n1))
+console.log(fn(o1).inspect())
